db695ed4e3b2e73a4707724f7c4e354456629a27
ahmedali2 <madzilla2005@hotmail.com>
2013-10-30 23:10:12 +0200

    new htmls

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 $(document).on("pageshow", function () { 
+
 	var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
 	var height = (window.innerHeight > 0) ? window.innerHeight : screen.height;
 	$('.tag').css('height', (height-90));
@@ -9,9 +10,76 @@ $(document).on("pageshow", function () {
 	$('.submit').css('top',(height-90));
 	$('#p_goal').hide();
 	$('#p_target').hide();
+	$('#p_about').show();
+	adjustTabs();
+
+
+	$(".h_tabs").on("mousedown mouseup", function(e){
+  	  $(this).toggleClass( "active", e.type === "mousedown" );
+	});
+
+	$('#t_about').click(function(){
+		$('#p_goal').hide();
+		$('#p_target').hide();
+		$('#p_about').fadeIn(500);
+		adjustTabs();
+	});
+
+	$('#t_goal').click(function(){
+		$('#p_about').hide();
+		$('#p_target').hide();
+		$('#p_goal').fadeIn(500);
+		adjustTabs();
+	});	
+
+	$('#t_target').click(function(){
+		$('#p_about').hide();
+		$('#p_goal').hide();
+		$('#p_target').fadeIn(500);
+		adjustTabs();
+	});	
+	
 });
 
-	
+function adjustTabs(){
+
+	if($('#p_goal').css('display')!='none'){
+		//Goal is Selected
+		$('#t_goal').css('background-image'," url(./icons/clicked.png)");
+	} else {
+		$('#t_goal').css('background-image',"none");
+	}
+
+	if($('#p_target').css('display')!='none'){
+		//target is Selected
+		$('#t_target').css('background-image'," url(./icons/clicked.png)");
+	} else {
+		$('#t_target').css('background-image',"none");
+	}
+
+	if($('#p_about').css('display')!='none'){
+		//about is Selected
+		$('#t_about').css('background-image'," url(./icons/clicked.png)");
+	} else {
+		$('#t_about').css('background-image',"none");
+	}
+}
+
+function toast(msg){
+	$("<div class='ui-loader ui-overlay-shadow ui-body-c ui-corner-all'><h3 style='color:#616161;'>"+msg+"</h3></div>")
+	.css({ display: "block", 
+		opacity: 0.90, 
+		position: "fixed",
+		padding: "7px",
+		"text-align": "center",
+		width: "270px",
+		left: ($(window).width() - 284)/2,
+		top: $(window).height()/2 })
+	.appendTo( $.mobile.pageContainer ).delay( 1500 )
+	.fadeOut( 400, function(){
+		$(this).remove();
+	});
+}
 $(document).ready(function() {		
 
 	//For handling the home page bottom tabs (background buttons..etc)
@@ -19,30 +87,6 @@ $(document).ready(function() {
 	$('#p_target').hide();
 
 	adjustTabs();
-	function adjustTabs(){
-
-		if($('#p_goal').css('display')!='none'){
-			//Goal is Selected
-			$('#t_goal').css('background-image'," url(./icons/clicked.png)");
-		} else {
-			$('#t_goal').css('background-image',"none");
-		}
-
-		if($('#p_target').css('display')!='none'){
-			//target is Selected
-			$('#t_target').css('background-image'," url(./icons/clicked.png)");
-		} else {
-			$('#t_target').css('background-image',"none");
-		}
-
-		if($('#p_about').css('display')!='none'){
-			//about is Selected
-			$('#t_about').css('background-image'," url(./icons/clicked.png)");
-		} else {
-			$('#t_about').css('background-image',"none");
-		}
-	}
-	
 
 	$(".h_tabs").on("mousedown mouseup", function(e){
   	  $(this).toggleClass( "active", e.type === "mousedown" );
@@ -78,5 +122,67 @@ $(document).ready(function() {
 	console.log('the header div: ',$('.theHeader').height());	
 	console.log('the card div: ',$('.card').height());
 
+	//----------- Message -------------
+	$('.submit').attr('disabled','disabled');
+
+	$('.submit').click(function(){
+		if($('.submit').attr('disabled') == 'disabled'){
+			console.log('its disabled');
+			 toast('Please type your email and a message..');
+		} else {
+			console.log('its enabled');
+			send();
+		}
+	});
+	
+	$('#c_email').keyup(function(){
+		enableSubmit();
+	});
+
+	$('#c_message').keyup(function(){
+		enableSubmit();
+	});
+	
+	function enableSubmit(){
+		if($('#c_email').val().trim()!='' && $('#c_message').val().trim()!= '') {
+			$('.submit').removeAttr('disabled');
+		}else{
+			$('.submit').attr('disabled','disabled');
+		}
+	}
+
+	
+	
+	function send(){
+		$.ajax({  
+			beforeSend: function() { $.mobile.showPageLoadingMsg(); }, //Show spinner
+            complete: function() { $.mobile.hidePageLoadingMsg() }, //Hide spinner
+			type: "POST",  
+			url: "http://teachinsquare.com/expo/send.php",  
+			data: { 'email': $('#c_email').val(), 'subject':$('#c_sub').val(), 'message': $('#c_message').val()} ,
+			success: function(data){  
+			
+				var response = JSON.parse(data);
+				
+				if(response.status){
+					console.log(response.status);
+
+					if(response.status=="success"){
+						//Show 'Message Sent!'
+						toast('Message Sent!');			
+						$('#c_email').val('');
+						$('#c_sub').val('');
+						$('#c_message').val('');
+					} else if(response.status=="error"){
+						//Show 'Sending Message failed try again later'
+						toast('Sending Message failed try again later.');
+					}					
+				} else {
+					//Show alert about the error
+					toast('Sending Message failed try again later.');
+				}
+			} 
+		});
+	}
 			
 }); 
